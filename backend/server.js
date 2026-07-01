3bc601d68ba29550093131fca351443cd5bc6958
Jessica Lopes <111246373+Jessicalopes02@users.noreply.github.com>
2026-07-01 17:14:31 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9099,7 +9099,7 @@ Object.entries(teams).forEach(([teamKey, users]) => {
     const manualAdjustments = {
   ferrari: 350,
   redbull:760,
-  mercedes:300
+  mercedes:640
 };
 
 // aplica ajuste ANTES do ranking
